79dd0ca626b8981ad9f66e59eb73285f180ea429
Nikola Jankovic <jankulica21@gmail.com>
2022-04-12 13:59:39 +0200

    Test

--- API/Startup.cs
+++ API/Startup.cs
@@ -27,6 +27,7 @@ namespace API
 
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        //Nije bitan redosled uvodjenja servisa
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -39,10 +40,12 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
+            services.AddCors();
 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //  VEOMA BITAN REDOSLED UVODJENJA
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -55,6 +58,9 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            
+            //Moguca konfiguracija
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
